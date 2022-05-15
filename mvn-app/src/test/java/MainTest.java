6f6847d1638554bd428334cacb9fd124652c48d7
pavelhaberzettl <pavel.haberzettl@seznam.cz>
2022-05-15 18:20:37 +0200

    nefunkcni test

--- mvn-app/src/test/java/MainTest.java
+++ mvn-app/src/test/java/MainTest.java
@@ -6,15 +6,19 @@ public class MainTest {
 
     @Test
     public void testDivideOk () {
-
         Main main = new Main();
         assertEquals(2, main.divide(4,2));
     }
 
     @Test
     public void testDivideFail () {
-
         Main main = new Main();
         assertEquals(3, main.divide(6,2));
+    }
+
+    @Test
+    public void testDivideError() {
+        Main main = new Main();
+        assertEquals(0, main.divide(6,2));
     }
 }
