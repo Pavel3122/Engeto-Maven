49b3787979566296e7a78c97b56047a77c306282
pavelhaberzettl <pavel.haberzettl@seznam.cz>
2022-05-15 17:25:35 +0200

    tests added

--- mvn-app/src/main/java/Main.java
+++ mvn-app/src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
 
+    public int divide (int i1, int i2) {
+        return i1 / i2;
+    }
+
     public static void main(String[] args){
         System.out.println("Hello world!!");
     }
---
+++ mvn-app/src/test/java/MainTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testDivideOk () {
+
+        Main main = new Main();
+        assertEquals(2, main.divide(4,2));
+    }
+
+    @Test
+    public void testDivideFail () {
+
+        Main main = new Main();
+        assertEquals(3, main.divide(6,2));
+    }
+}
